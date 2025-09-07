f9c489cc37bb2daeebaa5d161103eb08be3300b4
Hanzhen Yi <hanzhen.m.yi@gmail.com>
2025-09-07 12:45:32 +0900

    remove vite code from the template

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        playbround: resolve(__dirname, "playground/index.html"),
-      },
-    },
-  },
 });
